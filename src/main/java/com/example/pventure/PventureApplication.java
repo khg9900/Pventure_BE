220831efe001de619e6bab3339e8acdf11347d71
rabitis99 <kyungsu9914@gmail.com>
2025-10-17 14:49:56 +0900

    feat(docs): 도메인 및 글로벌 구조 README 작성

---
+++ src/main/java/com/example/pventure/PventureApplication.java
@@ -0,0 +1,13 @@
+package com.example.pventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PventureApplication.class, args);
+    }
+
+}
