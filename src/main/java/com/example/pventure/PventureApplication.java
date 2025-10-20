65765d31b79eaad28ee18007be0ded919671ccd7
rabitis99 <kyungsu9914@gmail.com>
2025-10-20 16:58:05 +0900

    [FEAT] 환경설정 정리 및 초기 DB 데이터 삽입

--- src/main/java/com/example/pventure/PventureApplication.java
+++ src/main/java/com/example/pventure/PventureApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class PventureApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PventureApplication.class, args);
     }
 
