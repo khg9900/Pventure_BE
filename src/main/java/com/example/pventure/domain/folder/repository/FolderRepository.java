4e4480b3e3008559b0dc62a6402bb2d8dbb3297c
rabitis99 <kyungsu9914@gmail.com>
2025-10-22 16:55:51 +0900

    [FIX] Repository: MemberRepository.findByTrip(Trip trip) 추가하여 Trip 전체 멤버 조회 지원, FolderRepository.findDefaultFolderByUser(User user) 추가하여 기본 폴더 조회 지원

--- src/main/java/com/example/pventure/domain/folder/repository/FolderRepository.java
+++ src/main/java/com/example/pventure/domain/folder/repository/FolderRepository.java
@@ -1,7 +1,15 @@
 package com.example.pventure.domain.folder.repository;
 
 import com.example.pventure.domain.folder.entity.Folder;
+import com.example.pventure.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+    @Query("SELECT f FROM Folder f WHERE f.user = :user AND f.isDefault = true")
+    Optional<Folder> findDefaultFolderByUser(@Param("user") User user);
 }
--- src/main/java/com/example/pventure/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/pventure/domain/member/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.example.pventure.domain.member.repository;
 
 import com.example.pventure.domain.member.entity.Member;
+import com.example.pventure.domain.trip.entity.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByTrip(Trip trip);
 }
