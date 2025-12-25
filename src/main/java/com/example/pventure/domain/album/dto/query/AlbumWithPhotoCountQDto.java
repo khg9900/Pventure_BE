624c39083e26c617fef55281bf96664b1ea8765d
khg9900 <whale4100@gmail.com>
2025-12-25 14:12:48 +0900

    fix: 사용하지 않는 import 제거

--- src/main/java/com/example/pventure/domain/album/dto/query/AlbumWithPhotoCountQDto.java
+++ src/main/java/com/example/pventure/domain/album/dto/query/AlbumWithPhotoCountQDto.java
@@ -1,10 +1,7 @@
 package com.example.pventure.domain.album.dto.query;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
